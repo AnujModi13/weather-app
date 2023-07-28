ac3e076c734a8768067cbb33a9251f66962219b1
Anuj <modianuj1376@gmail.com>
2023-07-28 17:57:42 +0530

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production:false,
-  baseUrl:"https://weatherapi-com.p.rapidapi.com/current.json",
-  XRapidAPIKeylabel:"X-RapidAPI-Key",
-  XRapidAPIKeyvalue:"8369c10dc8msh57903953c2fdf8ap1c3c63jsn8783d8c2d2ab",
-  XRapidAPIHostlabel:"X-RapidAPI-Host",
+  baseUrl:'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIKeylabel:'X-RapidAPI-Key',
+  XRapidAPIKeyvalue:'8369c10dc8msh57903953c2fdf8ap1c3c63jsn8783d8c2d2ab',
+  XRapidAPIHostlabel:'X-RapidAPI-Host',
   XRapidAPIHostvalue:"weatherapi-com.p.rapidapi.com",
 };
